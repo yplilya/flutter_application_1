d51bfc979fe99631a8dbf8d4f19ae36562f20c41
yplilya <ypl2002ilya@gmail.com>
2021-03-30 09:03:23 +0200

    Revert "Made the snake and navigetion"
    This reverts commit cb5a67f0430ed8d8ddc419ad39ccca683c46b5f2.

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'tic_tac_toe.dart';
-import 'snake.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(T_T_T());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey[900],
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: RaisedButton(
-                child: Text(
-                  'TIC TAC TOE',
-                  style: TextStyle(fontSize: 60, color: Colors.white),
-                ),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => TicTacToe()));
-                },
-              ),
-            ),
-            Expanded(
-              child: TextButton(
-                child: Text(
-                  'SNAKE',
-                  style: TextStyle(fontSize: 60, color: Colors.white),
-                ),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Snake()));
-                },
-              ),
-            ),
-          ],
-        ),
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: Column(
+        children: <Widget>[Expanded(child: Container())],
       ),
     );
   }
